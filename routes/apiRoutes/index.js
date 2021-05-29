9ee89f73fbeb698fdebcae06fa97dff96cb12de6
CastroOlympias <blackcarrera@msn.com>
2021-05-28 20:18:26 -0600

    cleanup up server code, used apiRoutes index.js and htmlRoutes to serve the front end, using status to make asses readily available, Code Modularized

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,16 @@
-const router = require('express').Router();
-const animalRoutes = require('../apiRoutes/animalRoutes');
+const router = require("express").Router();
+const notes = require('../../db/db');
+
+router.post('/notes', (req, res,) => {
+    const newNote = req.body;
+    console.log(newNote)
+  
+    return res.json(newNote);
+  });
+  
+  router.get('/notes', (req, res) => {
+    res.json(notes);
+  });
 
 
-router.use(animalRoutes);
-router.use(require('./zookeeperRoutes'));
-
-module.exports = router;
+module.exports  = router;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// Dependencies
-// =============================================================
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const express = require('express');
 const path = require('path');
 
 
 const app = express();
-const notes = require('./Develop/db/db');
 const fs = require('fs');
 const PORT = 3001;
 
@@ -13,61 +13,12 @@ const PORT = 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
-// =============================================================
+// express.static makes the html assests reedily available
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-});
-
-app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
-  console.log(newNote)
-
-  // notes.push(newNote);
-  newNotes = fs.writeFileSync(
-    path.join(__dirname, './Develop/db/db.json'),
-    JSON.stringify({ newNote }, null, 2)
-  );
-
-  res.json(newNote);
-});
-
-app.get('/api/notes', (req, res) => {
-  return res.json(notes);
-});
-
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-});
-
-// function findById(title, notes) {
-//   const result = notes.filter(notes => notes.title === title)[0];
-//   return result;
-// }
-
-// function createNewAnimal(body, notes) {
-//   const note = body;
-//   notes.push(note);
-//   fs.writeFileSync(
-//     path.join(__dirname, './Develop/db/db.json'),
-//     JSON.stringify({ animals: notes }, null, 2)
-//   );
-//   return animal;
-// }
-
-
-// Listener
-// =============================================================
-// createNewAnimal();
-// findById();
-
+// server start listener
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
 });
