775f98d1a3660f8c2b963c6e0aa0cbc9adf2fba5
CastroOlympias <blackcarrera@msn.com>
2021-05-28 21:32:09 -0600

    clean save

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,23 @@
 const router = require("express").Router();
 const notes = require('../../db/db');
 const fs = require('fs');
+const path = require('path');
 
-router.post('/notes', (req, res,) => {
-    const newNote = req.body;
-    console.log(newNote)
-  
-    res.json(notes);
-  });
+
   
   router.get('/notes', (req, res) => {
     res.json(notes);
   });
 
+  router.post('/notes', (req, res,) => {
+    const newNotes = req.body;
+
+    fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify({ notes: newNotes }, null, 2)
+      );
+  
+    return res.json(newNotes);
+  });
 
 module.exports  = router;
