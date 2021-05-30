0d1e806862c4b8b0f57ff751691b1dc3da7e2c93
Anomalous Alpha <blackcarrera@msn.com>
2021-05-29 19:12:42 -0600

    Finally another breakthrough. Created a dbWrite.js file in a lib directory to pass function through the router index.js file. borrowed code from so-zoo-me zoo app. Now can POST to create multiple entries in the db.json file. and GET with insomnia and webpage, no need to restart to see updated json data. inclueds id incrementer

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,25 +1,37 @@
 
 const router = require("express").Router();
-const notes = require('../../db/db.json');
+
 const fs = require('fs');
 const path = require('path');
+// const {uuid} = require('uuidv4');
 
-
+const {createNewZookeeper, validateZookeeper} = require('../../lib/dbWriter.js')
+const { zookeepers } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-    
-    return res.json(notes);
+    let results = zookeepers;
+    res.json(results);
 });
 
-router.post('/notes', (req, res,) => {
-    const newNotes = req.body;
+router.post('/notes', (req, res) => {
+    req.body.id = zookeepers.length.toString();
+  
+    if (!validateZookeeper(req.body)) {
+      res.status(400).send("The zookeeper is not properly formatted.");
+    } else {
+      const zookeeper = createNewZookeeper(req.body, zookeepers);
+      res.json(zookeeper);
+    }
+  });
 
-    fs.writeFileSync(
-        path.join(__dirname, "../../db/db.json"),
-        JSON.stringify( newNotes, null, 2)
-    );
-
-    return res.json(newNotes);
-});
+// router.post('/notes', (req, res,) => {
+//     // const notes = req.body
+//     req.body.id = notes.length.toString();
+//     fs.writeFileSync(
+//         path.join(__dirname, "../../db/db.json"),
+//         JSON.stringify(notes, null, 2)
+//     );
+//     res.json(notes);
+// });
 
 module.exports = router;
