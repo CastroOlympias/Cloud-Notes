fae3e4652f99d7b83a0d75083ac12d4fc0e003af
Anomalous Alpha <blackcarrera@msn.com>
2021-05-29 19:57:31 -0600

    removed references from zookeepers code and changed to notes and newNotes code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,21 +1,19 @@
 const router = require("express").Router();
-const {createNewZookeeper, validateZookeeper} = require('../../lib/dbWriter.js')
-const { zookeepers } = require('../../db/db.json');
+const createNotes = require('../../lib/dbWriter.js')
+const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-    let results = zookeepers;
+    let results = notes;
     res.json(results);
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = zookeepers.length.toString();
+    req.body.id = notes.length.toString();
   
-    if (!validateZookeeper(req.body)) {
-      res.status(400).send("The zookeeper is not properly formatted.");
-    } else {
-      const zookeeper = createNewZookeeper(req.body, zookeepers);
-      res.json(zookeeper);
-    }
+
+      const newNotes = createNotes(req.body, notes);
+      res.json(newNotes);
+    
   });
 
 module.exports = router;
