efdcf6cfb5ce72327db5d641b55cfa9640ab4dbe
Anomalous Alpha <blackcarrera@msn.com>
2021-05-29 20:34:47 -0600

    found code online, can delete notes from client app, but notes do note delete from the db.json file so notes come back after server restart

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,17 +3,25 @@ const createNotes = require('../../lib/dbWriter.js')
 const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
+  let results = notes;
+  res.json(results);
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-  
+  req.body.id = notes.length.toString();
 
-      const newNotes = createNotes(req.body, notes);
-      res.json(newNotes);
-    
-  });
+
+  const newNotes = createNotes(req.body, notes);
+  res.json(newNotes);
+
+});
+
+router.delete('/notes/:id', (req, res) => {
+  let deleteId = req.params.id;
+  let deleteObj = notes.find(user => user.id == deleteId);
+  let deleteIndex = notes.indexOf(deleteObj);
+  notes.splice(deleteIndex, 1);
+  res.send(deleteObj);
+});
 
 module.exports = router;
